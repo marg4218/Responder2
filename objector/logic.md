0cb9025f1f4aff6a103c772cee2af84c854c27a0
marg4218 <emarg4218@gmail.com>
2023-11-11 14:18:46 -0500

    Begin reorginization

---
+++ objector/logic.md
@@ -0,0 +1,9 @@
+
+Stage A: outlining the doc
+
+(1) Rogs, RPDs, RFAs
+(2) Count
+
+-- Build Views --
+
+No. Obj-List
---
+++ objector/objections-list.js
@@ -0,0 +1,19 @@
+const objections_array = [
+    'protected by the attorney-client privilege',
+    'not reasonably calculated to lead to the discovery of admissible evidence',
+
+    '',
+]
+
+
+
+
+//objections_array.forEach(/*code*/);
+
+const offerObjections = () => {
+
+};
+
+
+
+
